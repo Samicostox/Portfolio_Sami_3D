70673f57db72c5f057389405548d921bee36469b
uobjuniorsolutions <uonjuniorsolutions@gmail.com>
2024-04-29 19:41:02 +0100

    update routing

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -8,7 +8,7 @@ import { TracingBeam } from "./Beams/tracing-beam.tsx";
 export function TracingBeamDemo() {
   return (
     <TracingBeam className="px-6">
-      <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+      <div className="max-w-3xl mx-auto antialiased pt-4 relative">
         {dummyContent.map((item, index) => (
           <div key={`content-${index}`} className="mb-10">
             <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
